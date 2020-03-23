b69070d399ebbc5ef1aa75922fa7b143ec25e2f9
892420824@qq.com <892420824@qq.com>
2020-03-23 14:25:46 +0800

    修改pom文件配置

--- learn_jmm/learn_threadPool/src/main/java/com/luop/ThreadPoolDemo.java
+++ learn_jmm/learn_threadPool/src/main/java/com/luop/ThreadPoolDemo.java
@@ -1,8 +1,6 @@
 package com.luop;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * 创建线程的三种方式
@@ -11,16 +9,39 @@ public class ThreadPoolDemo {
     public static void main(String[] args) {
 //        test1();
 //        test2();
-        test3();
+//        test3();
+        test4();
+    }
+
+    //自定义线程池
+    private static void test4() {
+        int processors = Runtime.getRuntime().availableProcessors();   //cpu处理器核数
+        ExecutorService pool = new ThreadPoolExecutor(
+                3,
+                processors + 1,   //一般是处理器核数+1
+                60, TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy()     //执行策略，由调用者执行
+        );
+        try {
+            for (int i = 0; i < 13; i++) {
+                pool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+                });
+            }
+        } finally {
+            pool.shutdown();
+        }
     }
 
     //创建带缓存的线程池
     private static void test3() {
-        ExecutorService pool = Executors.newCachedThreadPool();
+        ExecutorService pool = Executors.newCachedThreadPool();   //具体的线程数量取决于系统分配
         try {
             for (int i = 0; i < 10; i++) {
-                pool.execute(() ->{
-                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
+                pool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                     try {
                         TimeUnit.MILLISECONDS.sleep(100);  //模拟业务办理
                     } catch (InterruptedException e) {
@@ -40,8 +61,8 @@ public class ThreadPoolDemo {
         ExecutorService pool = Executors.newSingleThreadExecutor();
         try {
             for (int i = 0; i < 10; i++) {
-                pool.execute(() ->{
-                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
+                pool.execute(() -> {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                     try {
                         TimeUnit.MILLISECONDS.sleep(200);  //模拟业务办理
                     } catch (InterruptedException e) {
@@ -62,7 +83,7 @@ public class ThreadPoolDemo {
         try {
             for (int i = 0; i < 10; i++) {
                 pool.execute(() -> {
-                    System.out.println(Thread.currentThread().getName()+"\t 办理业务");
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
                     try {
                         TimeUnit.MILLISECONDS.sleep(300);   //模拟业务执行
                     } catch (InterruptedException e) {
@@ -70,9 +91,9 @@ public class ThreadPoolDemo {
                     }
                 });
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally{
+        } finally {
             pool.shutdown();   //关闭线程池
         }
     }
